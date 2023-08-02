90f6e77c131108c41cb7ca2b10e776f4f81dafeb
music-tko <thando@vaangroup.com>
2023-08-02 22:46:59 +0200

    Edits

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -5,14 +5,20 @@ import {info} from "../../info/Info";
 
 export default function Portfolio() {
     return (
-        <Box>
-            <Grid container display={'flex'} justifyContent={'center'}>
-                {info.portfolio.map((project, index) => (
-                   <Grid item xs={12} md={6} key={index}>
-                       <PortfolioBlock image={project.image} live={project.live} source={project.source} title={project.title} />
-                   </Grid>
-                ))}
-            </Grid>
-        </Box>
+		<Box>
+			<h4>
+				Throughout my freelancing and agency career,  <br />
+				I've collaborated with great clients on amazing projects.  <br />
+				Though I can't showcase everything publicly, I'd be happy to share tailored examples upon request. <br />
+				Let's connect!
+			</h4>
+			<Grid container display={'flex'} justifyContent={'center'} flexWrap={'wrap'} alignContent={'center'}>
+				{info.portfolio.map((project, index) => (
+					<Grid item xs={6} md={3} key={index}>
+						<PortfolioBlock image={project.image} />
+					</Grid>
+				))}
+			</Grid>
+		</Box>
     );
 };
--- src/info/Info.js
+++ src/info/Info.js
@@ -4,85 +4,64 @@ import mock2 from "../img/mock2.png"
 import mock3 from "../img/mock3.png"
 import mock4 from "../img/mock4.png"
 import mock5 from "../img/mock5.png"
+import mock6 from "../img/mock6.svg"
+import mock7 from "../img/mock7.png"
+import mock8 from "../img/mock8.webp"
+import mock9 from "../img/mock9.svg"
+import mock10 from "../img/mock10.png"
+import mock11 from "../img/mock11.png"
+import mock12 from "../img/mock12.png"
 
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
-
-export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
-
-
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
+export let colors = ["rgb(255, 105, 180)", "rgb(255, 105, 180)", "rgb(0, 0, 0)"];
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a Full Stack Developer",
-    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    firstName: "Thando",
+    lastName: "Khoase",
+    initials: "TK", 
+    position: "a Frontend Engineer",
+    selfPortrait: self, 
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, 
     baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    miniBio: [
         {
-            emoji: '☕',
-            text: 'fueled by coffee'
+            emoji: '💻 ',
+            text: 'fueled by challenges (corny but true)'
         },
         {
             emoji: '🌎',
-            text: 'based in the US'
+            text: 'based in the South Africa'
         },
         {
             emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: "Frontend Engineer"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "thandokhoase19@gmail.com"
         }
     ],
     socials: [
         {
-            link: "https://facebook.com",
-            icon: 'fa fa-facebook',
-            label: 'facebook'
-        },
-        {
-            link: "https://instagram.com",
-            icon: 'fa fa-instagram',
-            label: 'instagram'
-        },
-        {
-            link: "https://github.com",
+            link: "https://github.com/music-tko",
             icon: "fa fa-github",
             label: 'github'
         },
         {
-            link: "https://linkedin.com",
+            link: "https://www.linkedin.com/in/thay-khoase026/",
             icon: "fa fa-linkedin",
             label: 'linkedin'
         },
         {
-            link: "https://twitter.com",
+            link: "https://twitter.com/ThayDevToBe",
             icon: "fa fa-twitter",
             label: 'twitter'
         }
-// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-// Just change the links so that they lead to your social profiles.
-
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hello! I'm Thando. I'm a Frontend Engineer. I'm selftaught, I enjoy reading all types of books and playing all times of pc games, and I am always looking to learn more. You should hire me!",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+            proficientWith: ['javascript', 'typescript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma', 'wordpress php', 'shopify liquid', 'tailwindcss', 'jQuery', 'redux'],
+            exposedTo: ['php', 'firebase']
         }
     ,
     hobbies: [
@@ -99,42 +78,47 @@ export const info = {
             emoji: '🎥'
         },
         {
-            label: 'cooking',
-            emoji: '🌶'
+            label: 'music',
+            emoji: '🎵'
         }
-// Same as above, change the emojis to match / relate to your hobbies or interests.
-// You can also remove the emojis if you'd like, I just think they look cute :P
+
     ],
-    portfolio: [ // This is where your portfolio projects will be detailed
+    portfolio: [
         {
-            title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
         {
-            title: "Project 2",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
             image: mock2
         },
         {
-            title: "Project 3",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock3
+            image: mock9
         },
         {
-            title: "Project 4",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
             image: mock4
         },
         {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            image: mock7
+        },
+        {
+            image: mock6
+        },
+        {
+            image: mock8
+        },
+        {
+            image: mock3
+        },
+        {
             image: mock5
+        },
+        {
+            image: mock10
+        },
+        {
+            image: mock11
+        },
+        {
+            image: mock12
         }
     ]
 }
